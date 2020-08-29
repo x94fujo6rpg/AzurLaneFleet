5b4cfa8933576e3f5eae3b3a3f8ab1c11ba2c899
x94fujo6rpg <x94fujo6rpg@gmail.com>
2020-08-30 05:29:58 +0800

    dev update

--- js/main-v.js
+++ js/main-v.js
@@ -1,66 +1,71 @@
 Vue.component("item-container", {
   props: ["item", "lang"],
   template: `
-    <button
-      class="container-fluid p-1 bg-transparent border-0" 
-      v-bind:name="item.id"
-      v-bind:pos="item.property.pos"
-      onclick="setCurrent(this)"
-      data-toggle="modal"
-      v-bind:data-target="item.property.target"
-    >
-      <div class="container-fluid p-0">
-        <div class="icon_box">
-          <img class="img-fluid icon" v-bind:src="item.property.icon">
-          <img class="img-fluid bg" v-bind:src="item.property.bg">
-          <img class="img-fluid frame" v-bind:src="item.property.frame">
-        </div>
-        <span class="d-flex justify-content-center text-truncate item_name"
-          v-text="item.property[lang]"
-        ></span>
-      </div>
-    </button>
-  `
+        <button
+          class="p-1 item_container" 
+
+          v-bind:name="item.id"
+          v-bind:pos="item.property.pos"
+          v-bind:data-target="item.property.target"
+
+          onclick="setCurrent(this)"
+          data-toggle="modal"          
+          >
+            <div class="container-fluid p-0">
+              <div class="icon_box">
+                <img class="img-fluid icon" v-bind:src="item.property.icon">
+                <img class="img-fluid bg" v-bind:src="item.property.bg">
+                <img class="img-fluid frame" v-bind:src="item.property.frame">
+              </div>
+              <span class="d-flex justify-content-center text-truncate item_name"
+                v-text="item.property[lang]"
+              ></span>
+            </div>
+        </button>
+    `,
 });
+
+
+//class="container-fluid p-1 bg-transparent border-0" 
 
 Vue.component("ship-container", {
   props: ["ship", "lang"],
   template: `
-    <div class="d-flex flex-row">
-        <item-container
-            v-for="item in ship.item"
-            v-bind:key="item.id"
-            v-bind:item="item"
-            v-bind:lang="lang"
-        ></item-container>
-    </div>
-  `
+        <div class="col">
+            <item-container
+                v-for="item in ship.item"
+                v-bind:key="item.id"
+                v-bind:item="item"
+                v-bind:lang="lang"
+            ></item-container>
+        </div>
+    `
 });
 
 Vue.component("fleet-container", {
   props: ["fleet", "lang"],
   template: `
-    <div class="container-fluid row m-0 p-3"">
-      <div class="col">
-          <ship-container 
-              v-for="back_ship in fleet.back_ship" 
-              v-bind:key="back_ship.id" 
-              v-bind:ship="back_ship"
-              v-bind:name="back_ship.id"
-              v-bind:lang="lang"
-          ></ship-container>
-      </div>
-      <div class="col">
-          <ship-container 
-              v-for="front_ship in fleet.front_ship" 
-              v-bind:key="front_ship.id" 
-              v-bind:ship="front_ship"
-              v-bind:name="front_ship.id"
-              v-bind:lang="lang"
-          ></ship-container>
-      </div>
-    </div>
-  `
+        <div class="row m-auto">
+            <div class="flex-col m-auto">
+                <ship-container 
+                    v-for="back_ship in fleet.back_ship" 
+                    v-bind:key="back_ship.id" 
+                    v-bind:ship="back_ship"
+                    v-bind:name="back_ship.id"
+                    v-bind:lang="lang"
+                ></ship-container>
+            </div>
+            <div class="flex-col m-auto">
+                <ship-container 
+                    v-for="front_ship in fleet.front_ship" 
+                    v-bind:key="front_ship.id" 
+                    v-bind:ship="front_ship"
+                    v-bind:name="front_ship.id"
+                    v-bind:lang="lang"
+                ></ship-container>
+            </div>
+        </div>
+    `
 });
 
 Vue.component("ship-nation-button", {
@@ -70,9 +75,9 @@ Vue.component("ship-nation-button", {
       type="button"
       class="btn btn-outline-light btn-sm"
       onclick="shipDisplay(this)"
-    ></button>
+      ></button>
   `
-});
+})
 
 Vue.component("ship-type-button", {
   props: ['type', "lang"],
@@ -81,9 +86,9 @@ Vue.component("ship-type-button", {
       type="button"
       class="btn btn-outline-light btn-sm"
       onclick="shipDisplay(this)"
-    ></button>
+      ></button>
   `
-});
+})
 
 Vue.component("ship-rarity-button", {
   props: ['rarity', "lang"],
@@ -92,25 +97,49 @@ Vue.component("ship-rarity-button", {
       type="button"
       class="btn btn-outline-light btn-sm"
       onclick="shipDisplay(this)"
-    ></button>
+      ></button>
   `
-});
+})
 
 Vue.component("ship-list-button", {
   props: ["shiplist", "item", "lang"],
   template: `
   <button
-    class="p-1 item_container" 
-    style="width:100px"
+    class="p-1 item_container"
+
+    onclick="setShipAndEquip(this)"
+    data-dismiss="modal"
+
+    v-bind:id="item.id"
+    >
+    <div class="container-fluid p-0">
+        <div class="icon_box row">
+          <img class="img-fluid icon" v-bind:src="item.icon">
+          <img class="img-fluid bg" v-bind:src="item.bg">
+          <img class="img-fluid frame" v-bind:src="item.frame">
+        </div>
+        <span class="row d-flex justify-content-center text-truncate item_name"
+          v-text="item[lang]"
+        ></span>
+    </div>
+  </button>
+  `
+})
+
+Vue.component("equip-list-button", {
+  props: ["equips", "item", "lang"],
+  template: `
+  <button
+    class="p-1 item_container"
     onclick="setShipAndEquip(this)"
     data-dismiss="modal"
     v-bind:id="item.id"
-  >
+    >
     <div class="container-fluid p-0">
-        <div class="icon_box">
-          <img class="img-fluid icon" v-bind:src="item.icon" loading="lazy">
-          <img class="img-fluid bg" v-bind:src="item.bg" loading="lazy">
-          <img class="img-fluid frame" v-bind:src="item.frame" loading="lazy">
+        <div class="container-fluid icon_box">
+          <img class="img-fluid bg" v-bind:src="item.bg">
+          <img class="img-fluid frame" v-bind:src="item.frame">
+          <img class="img-fluid eqicon" v-bind:src="item.icon">
         </div>
         <span class="d-flex justify-content-center text-truncate item_name"
           v-text="item[lang]"
@@ -118,7 +147,7 @@ Vue.component("ship-list-button", {
     </div>
   </button>
   `
-});
+})
 
 let [ship_nation, ship_type, ship_rarity] = buildShipSelectOption();
 let c_fleet = "";
@@ -133,9 +162,9 @@ let fleet_data = buildFleet();
 let sorted_ship_data = [];
 let lan = "";
 
-console.time("initial");
+let sorted_equip_data = [];
+
 initial();
-console.timeEnd("initial");
 
 let ALF = new Vue({
   el: "#AzurLaneFleetApp",
@@ -156,38 +185,63 @@ let shipSelect = new Vue({
   }
 });
 
+let equipSelect = new Vue({
+  el: "#equipselect",
+  data: {
+    equips: sorted_equip_data,
+    lang: lan,
+  }
+});
+
 function initial() {
   lan = "cn";
+
+  /*-------------------------------------------------------
+  creat sortred ship list
+  -------------------------------------------------------*/
+  console.time("sortship");
   let newlist = [];
   let pos = 0;
-  let emptyship = {};
+  let empty = {};
+  let parseData = {
+    id: "uni_id",
+    cn: "cn_name",
+    en: "en_name",
+    jp: "jp_name",
+    type: "type",
+    nationality: "nationality",
+    rarity: "rarity",
+    star: "star",
+    retro: "retro",
+    base: "base_list",
+    e1: "equip_1",
+    e2: "equip_2",
+    e3: "equip_3",
+    e4: "equip_4",
+    e5: "equip_5",
+  };
   for (let index in ship_data) {
     let item = Object.assign({}, ship_data[index]);
     let newitem = {};
-    let parseData = {
-      id: "uni_id",
-      cn: "cn_name",en: "en_name",jp: "jp_name",
-      type: "type",
-      nationality: "nationality",
-      rarity: "rarity",
-      star: "star"
-    };
-    for (let index in parseData) {
-      newitem[index] = item[parseData[index]];
+    // parse data
+    for (let key in parseData) {
+      newitem[key] = item[parseData[key]];
     }
+    // set other data
     newitem.icon = `shipicon/${item.painting.toLowerCase()}.png`;
     newitem.bg = `ui/bg${item.rarity - 1}.png`;
     newitem.frame = `ui/frame_${item.rarity - 1}.png`;
+    // creat empty ship
     if (pos === 0) {
-      emptyship = Object.assign({}, newitem);
-      for (let key in emptyship) {
-        emptyship[key] = "";
+      empty = Object.assign({}, newitem);
+      for (let key in empty) {
+        empty[key] = "";
       }
-      emptyship.id = "000000";
-      emptyship.en = "removeship";
-      emptyship.cn = "移除";
-      emptyship.jp = "除隊";
-      emptyship.icon = "ui/empty.png";
+      empty.id = "000000";
+      empty.en = "remove";
+      empty.cn = "移除";
+      empty.jp = "除隊";
+      empty.icon = "ui/empty.png";
     }
     newlist.push(newitem);
     pos++;
@@ -195,9 +249,63 @@ function initial() {
   newlist = sorting(newlist, 'type', true);
   newlist = sorting(newlist, 'nationality', true);
   newlist = sorting(newlist, 'rarity', true);
-
-  newlist.unshift(emptyship);
+  // add emptyship to top
+  newlist.unshift(empty);
   sorted_ship_data = Object.assign([], newlist);
+  console.timeEnd("sortship");
+  /*-------------------------------------------------------
+  creat sortred equip list
+  -------------------------------------------------------*/
+  console.time("sortequip");
+  newlist = [];
+  pos = 0;
+  parseData = {
+    id: "id",
+    cn: "cn_name",
+    en: "en_name",
+    jp: "jp_name",
+    type: "type",
+    nationality: "nationality",
+    rarity: "rarity",
+    bf: "ship_type_forbidden",
+  };
+  for (let index in equip_data) {
+    let item = Object.assign({}, equip_data[index]);
+    let newitem = {};
+    // parse data
+    for (let key in parseData) {
+      newitem[key] = item[parseData[key]];
+    }
+    // set other data
+    newitem.icon = `equips/${item.icon}.png`;
+    if (item.rarity != 1) {
+      newitem.bg = `ui/bg${item.rarity - 1}.png`;
+      newitem.frame = `ui/frame_${item.rarity - 1}.png`;
+    } else {
+      newitem.bg = `ui/bg${item.rarity}.png`;
+      newitem.frame = `ui/frame_${item.rarity}.png`;
+    }
+    // creat empty equip
+    if (pos === 0){
+      empty = Object.assign({}, newitem);
+      for (let key in empty) {
+        empty[key] = "";
+      }
+      empty.id = "666666";
+      empty.en = "remove";
+      empty.cn = "移除";
+      empty.jp = "外す";
+      empty.icon = "ui/empty.png";
+    }
+    newlist.push(newitem);
+    pos++;
+  }
+  newlist = sorting(newlist, "nationality", true);
+  newlist = sorting(newlist, "type", true);
+  newlist = sorting(newlist, "rarity", true);
+  newlist.unshift(empty);
+  sorted_equip_data = Object.assign([],newlist);
+  console.timeEnd("sortequip");
 }
 
 function setlang(item) {
@@ -214,17 +322,67 @@ function setShipAndEquip(item) {
       return Object.assign({}, ele);
     }
   });
-  let app_item = shipInApp.item[0].property;
-  let copyList = ["cn", "en", "jp", "icon", "frame", "bg"];
-  if (item.id === "000000") {
-    app_item.cn = "船";
-    app_item.en = "Ship";
-    app_item.jp = "キャラ";
-    app_item.icon = shipInList.icon;
-    app_item.frame = "";
-    app_item.bg = "";
-  } else {
-    copyList.forEach(key => app_item[key] = shipInList[key]);
+  let app_item = shipInApp.item;
+  let shipCopyList = ["cn", "en", "jp", "icon", "frame", "bg"];
+
+  for (let index in app_item) {
+    app_item = shipInApp.item[index].property;
+    let parsetype = {
+      1: { cn: "驅逐砲", en: "DD Gun", jp: "駆逐砲" },
+      2: { cn: "輕巡砲", en: "CL Gun", jp: "軽巡砲" },
+      3: { cn: "重巡砲", en: "CA Gun", jp: "重巡砲" },
+      4: { cn: "戰艦砲", en: "BB Gun", jp: "戦艦砲" },
+      5: { cn: "魚雷", en: "Torpedoe", jp: "魚雷" },
+      6: { cn: "防空砲", en: "Anti-Air Gun", jp: "対空砲" },
+      7: { cn: "戰鬥機", en: "Fighter", jp: "戦闘機" },
+      8: { cn: "攻擊機", en: "Torpedo Bomber", jp: "攻撃機" },
+      9: { cn: "爆擊機", en: "Dive Bomber", jp: "爆撃機" },
+      10: { cn: "設備", en: "Auxiliary", jp: "設備" },
+      11: { cn: "超巡砲", en: "CB Gun", jp: "超巡砲" },
+      12: { cn: "水上機", en: "Seaplane", jp: "水上機" },
+      13: { cn: "潛艇魚雷", en: "Submarine Torpedoe", jp: "潜水艦魚雷" },
+      14: { cn: "爆雷", en: "Depth Charge", jp: "爆雷" }, //Sonar is not a unique type
+      15: { cn: "反潛機", en: "ASW Bomber", jp: "対潜機" },
+      17: { cn: "直升機", en: "ASW Helicopter", jp: "ヘリ" },
+    };
+
+    if (item.id === "000000") {
+      if (index === "0") {
+        //app_item.cn = "船"; app_item.en = "Ship"; app_item.jp = "キャラ";
+        shipCopyList.forEach(key => app_item[key] = "");
+        app_item.icon = shipInList.icon;
+      } else {
+        shipCopyList.forEach(key => app_item[key] = "");
+        app_item.icon = "ui/icon_back.png";
+        app_item.fb = [];
+        app_item.type = [];
+      }
+    } else {
+      if (index === "0") {
+        shipCopyList.forEach(key => app_item[key] = shipInList[key]);
+      } else {
+        let typelist = shipInList[`e${index}`];
+        app_item.type = typelist;
+        app_item.icon = "ui/empty.png";
+        let typestr_cn = "";
+        let typestr_en = "";
+        let typestr_jp = "";
+        typelist.forEach((type, index) => {
+          typestr_cn += parsetype[type].cn;
+          typestr_en += parsetype[type].en;
+          typestr_jp += parsetype[type].jp;
+          if (typelist.length > 1 && index < typelist.length - 1) {
+            typestr_cn += "/";
+            typestr_en += "/";
+            typestr_jp += "/";
+          }
+        });
+        app_item.cn = typestr_cn;
+        app_item.en = typestr_en;
+        app_item.jp = typestr_jp;
+        app_item.target = "#equipselect";
+      }
+    }
   }
 }
 
@@ -268,9 +426,9 @@ function buildFleet() {
         type: "",
         star: "",
         rarity: "",
-        en: "Ship",
-        cn: "船",
-        jp: "キャラ",
+        en: "",
+        cn: "",
+        jp: "",
         target: "#shipselect",
         bg: "",
         frame: "",
@@ -279,13 +437,13 @@ function buildFleet() {
     } else {
       let eq = {
         icon: "ui/icon_back.png",
-        type: "",
+        type: [],
         star: "",
         rarity: "",
-        en: "Equip",
-        cn: "裝備",
-        jp: "装備",
-        target: "#eqipselect",
+        en: "",
+        cn: "",
+        jp: "",
+        target: "",
         bg: "",
         frame: "",
         fb: []
@@ -382,4 +540,3 @@ function buildShipSelectOption() {
   });
   return [nation, type, rarity];
 }
-
